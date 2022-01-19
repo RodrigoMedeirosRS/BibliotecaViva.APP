252a6de05de057d59eece71a40fe06acfc2a6a4b
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-01-19 15:16:56 -0300

    feat(implementação tipos de registro)

--- APP/CTRL/RegistroBoxCTRL.cs
+++ APP/CTRL/RegistroBoxCTRL.cs
@@ -2,15 +2,23 @@ using Godot;
 using System;
 
 using BibliotecaViva.DTO;
+using BibliotecaViva.DTO.Utils;
 using BibliotecaViva.CTRL.Interface;
 
 namespace BibliotecaViva.CTRL
 {
 	public class RegistroBoxCTRL : Panel, IDisposableCTRL
 	{
+		private bool Maximizado { get; set; }
 		private Label Nome { get; set; }
 		private Label Apelido { get; set; }
 		private RichTextLabel Descricao { get; set; }
+		private RichTextLabel ConteudoTextual { get; set; }
+		private ColorRect FundoBox { get; set; }
+		private RegistroDTO Registro { get; set; }
+		private Control CampoDescricao { get; set; }
+		private Control CampoImagem { get; set; }
+		private Control CampoTextual { get; set; }
 		public override void _Ready()
 		{
 			PopularNodes();
@@ -18,9 +26,16 @@ namespace BibliotecaViva.CTRL
 		}
 		private void PopularNodes()
 		{
+			Maximizado = false;
 			Nome = GetNode<Label>("./Nome");
 			Apelido = GetNode<Label>("./VBoxContainer/Apelido/Conteudo");
 			Descricao = GetNode<RichTextLabel>("./VBoxContainer/Descricao/ScrollContainer/Conteudo");
+			ConteudoTextual = GetNode<RichTextLabel>("./VBoxContainer/Texto/ScrollContainer/Conteudo");
+			
+			FundoBox = GetNode<ColorRect>("./Controladores/ColorRect");
+			CampoTextual = GetNode<Control>("./VBoxContainer/Texto");
+			CampoImagem = GetNode<Control>("./VBoxContainer/Imagem");
+			CampoDescricao = GetNode<Control>("./VBoxContainer/Descricao");
 		}
 		private void DesativarFuncoesDeAltoProcessamento()
 		{
@@ -30,6 +45,7 @@ namespace BibliotecaViva.CTRL
 		public void Preencher(RegistroDTO registroDTO, Vector2 posicao)
 		{
 			RectPosition = posicao;
+			Registro = registroDTO;
 			Nome.Text = registroDTO.Nome;
 			PopularCampoOpcional(Apelido, registroDTO.Apelido);
 			PopularCampoOpcional(Descricao, registroDTO.Descricao);
@@ -53,6 +69,67 @@ namespace BibliotecaViva.CTRL
 			
 		}
 		private void _on_Maximizar_button_up()
+		{
+			GD.Print(TipoExecucao.Audio.ToString() + " " + Registro.Tipo);
+			if (Maximizado)
+				ExibirDescricao();
+			else
+				ExibirCampo();
+			
+		}
+		private void ExibirCampo()
+		{
+			if (Registro.Tipo == TipoExecucao.Audio.ToString())
+			{
+				CampoTextual.Visible = false;
+				CampoDescricao.Visible = false;
+				CampoImagem.Visible = false;
+			}
+			else if (Registro.Tipo == TipoExecucao.Imagem.ToString())
+			{
+				CampoImagem.Visible = true;
+				CampoDescricao.Visible = false;
+				CampoTextual.Visible = false;
+			}
+
+			else if (Registro.Tipo == TipoExecucao.Texto.ToString())
+			{
+				
+				ExibirRegistroTextual();
+			}
+
+			else if (Registro.Tipo == TipoExecucao.Arquivo.ToString())
+			{
+				CampoDescricao.Visible = false;
+				CampoImagem.Visible = false;
+				CampoTextual.Visible = true;
+			}
+			else if (Registro.Tipo == TipoExecucao.URL.ToString())
+			{
+				CampoDescricao.Visible = false;
+				CampoImagem.Visible = false;
+				CampoTextual.Visible = true;
+			}
+		}
+		private void ExibirDescricao()
+		{
+			CampoDescricao.Visible = true;
+			CampoImagem.Visible = false;
+			CampoTextual.Visible = false;
+			FundoBox.RectSize = new Vector2(400, 303);
+			RectSize = new Vector2(400, 303);
+		}
+		private void ExibirRegistroTextual()
+		{
+			CampoTextual.Visible = true;
+			CampoDescricao.Visible = false;
+			CampoImagem.Visible = false;
+			FundoBox.RectSize = new Vector2(400, 535);
+			RectSize = new Vector2(400, 535);
+			
+			ConteudoTextual.Text = Registro.Conteudo;
+		}
+		private void ExibirRegistroImagem()
 		{
 			
 		}
